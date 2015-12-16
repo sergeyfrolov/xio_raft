05b41017eb0b2643eace26b96b419f04e967f523
Sergey Frolov <sergey.frolov@colorado.edu>
2015-12-16 15:04:27 -0700

    RaftLog logic innitial commit

--- raft_log.cpp
+++ raft_log.cpp
@@ -2,13 +2,31 @@
 // Use of this source code is governed by a OpenIB.org BSD license that can be
 // found in the LICENSE file.
 
+#include <unistd.h>
+#include <thread>
 #include "raft_log.h"
+#include "xio_callbacks.h"
+#include <sstream>
+#include <fstream>
 
-RaftLog::RaftLog() {
-    term = 0;
+using std::ifstream;
+using std::istringstream;
+
+
+RaftLog::RaftLog():
+        stateSharedLock(stateMutex, std::defer_lock), stateUniqueLock(stateMutex, std::defer_lock) {
+    currentTerm = 0;
     votes = 0;
-    changing_state = false;
 };
+
+void RaftLog::runXioLoop() {
+    for(int i = 1; i < nodesTotal; i++) {
+        if (i != id)
+            xio_context_run_loop(xio_session_data[i].ctx, 10);
+        else
+            xio_context_run_loop(xio_session_data[i].ctx, 100);
+    }
+}
 
 // typical singleton getInstance
 RaftLog& RaftLog::getInstance() {
@@ -17,8 +35,7 @@ RaftLog& RaftLog::getInstance() {
 }
 
 // State-related fields methods
-void RaftLog::setCurrentState(RaftStateInterface *s)
-{
+void RaftLog::setCurrentState(RaftStateInterface *s) {
     state = s;
 }
 void RaftLog::becomeFollower() {
@@ -31,6 +48,113 @@ void RaftLog::becomeLeader() {
     state->becomeLeader();
 };
 
+void RaftLog::applyAllUncommitedLogs() {
+    while (commitIndex > lastApplied) {
+        applyToStateMachine(lastApplied);
+        lastApplied++;
+    }
+}
+
+void RaftLog::initXio() {
+    xio_session_ops.on_session_event = xio_raft::on_session_event;
+    xio_session_ops.on_new_session = xio_raft::on_new_session;
+    xio_session_ops.on_msg_send_complete = NULL;
+    xio_session_ops.on_msg = xio_raft::on_request;
+    xio_session_ops.on_msg_error = NULL;
+
+    xio_cparams         = new xio_connection_params[nodesTotal];
+    xio_params          = new xio_session_params[nodesTotal];
+    xio_session_data    = new session_data[nodesTotal];
+    xio_sessions        = new xio_session*[nodesTotal];
+
+    xio_init();
+
+    /* Logging ... */
+    if (0) {
+        int level = XIO_LOG_LEVEL_DEBUG;
+        xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_LOG_LEVEL, &level, sizeof(level));
+    }
+
+    for(int i = 1; i < nodesTotal; i++) {
+        if (i != id) {
+            memset(&xio_session_data[i], 0, sizeof(xio_session_data[i]));
+            memset(&xio_params[i], 0, sizeof(xio_params[i]));
+            memset(&xio_cparams[i], 0, sizeof(xio_cparams[i]));
+
+            xio_session_data[i].ctx = xio_context_create(NULL, 0, -1);
+
+            xio_params[i].type = XIO_SESSION_CLIENT;
+            xio_params[i].ses_ops = &xio_session_ops;
+            xio_params[i].user_context = &xio_session_data[i];
+            xio_params[i].uri = nodeAddress[i].c_str();
+
+            xio_sessions[i] = xio_session_create(&xio_params[i]);
+
+            xio_cparams[i].session = xio_sessions[i];
+            xio_cparams[i].ctx = xio_session_data[i].ctx;
+            xio_cparams[i].conn_user_context = &xio_session_data[i];
+
+            /* connect the session  */
+            xio_session_data[i].conn = xio_connect(&xio_cparams[i]);
+        }
+        else
+        {
+            memset(&xio_session_data[i], 0, sizeof(xio_session_data[i]));
+
+            /* create thread context for the client */
+            xio_session_data[i].ctx = xio_context_create(NULL, 0, -1);
+            xio_server = xio_bind(xio_session_data[i].ctx, &xio_session_ops,
+                                  nodeAddress[id].c_str(), NULL, 0, &xio_session_data[i]);
+            if (xio_server) {
+                std::cout << "Bound to " << nodeAddress[id].c_str() << std::endl;
+            }
+            else {
+                std::cerr << "Error! Could not bind!" << std::endl;
+            }
+        }
+    }
+
+    //std::thread(&RaftLog::runXioLoop, this).detach();
+}
+
+void RaftLog::applyToStateMachine(uint64_t indexToApply) {
+    // Override me
+    cout << "Applying \"" << log[indexToApply].entry << "\" to State Machine. " <<
+            "EntryTerm = " << log[indexToApply].term << endl;
+    return;
+}
+
+int RaftLog::getState() {
+    return state->currentState;
+}
+
 RaftLog::~RaftLog() {
     delete state;
 };
+
+std::map<int, string> RaftLog::parse_config_file(string filename) {
+    ifstream ws;
+    ws.open(filename);
+    if (!ws.good()) {
+        throw std::runtime_error("Could not open " + filename);
+    }
+    char *line_buf = new char[1024];
+    std::map<int, string> parsed_dict;
+
+    while (!ws.eof()) {
+        ws.getline(line_buf, 1024);
+        if (line_buf[0]) {
+            istringstream iss(line_buf);
+            string var_name, var_value;
+            std::getline(iss, var_name, ' ');
+            std::getline(iss, var_value, '#');
+            if ((var_name[0] != '#') && (!var_value.empty()))
+                parsed_dict[stoi(var_name)] = var_value;
+        }
+    }
+    cout << "Parsed nodes from " << filename << endl;
+        for (auto const &it : parsed_dict)
+            cout << it.first << " - " << it.second << endl;
+        cout << endl;
+    return parsed_dict;
+}
--- raft_log.h
+++ raft_log.h
@@ -5,10 +5,25 @@
 #ifndef XIO_RAFT_RAFT_LOG_H
 #define XIO_RAFT_RAFT_LOG_H
 
-#include <array>
+#include "libxio.h"
+
+#include <vector>
 #include <string>
+#include <atomic>
+#include <shared_mutex>
+#include <forward_list>
+#include <map>
+#include <xio_base.h>
+
+#include "defines.h"
 #include "raft_state_interface.h"
 
+using std::string;
+
+class RaftStateInterface;
+
+struct session_data;
+struct logEntry;
 /* Due to accelio callbacks signatures, being pointers to non-class member functions,
  * we have to use static functions and global states.
  * Luckily, raft object is somewhat fine being a singleton.
@@ -16,28 +31,46 @@
 class RaftLog {
 protected:
     RaftStateInterface* state;
+    mutable std::shared_timed_mutex stateMutex; // make it shared_mutex when c++17 comes out -_-
 
     RaftLog();
     ~RaftLog();
 public:
+    std::map<int, std::string>    nodeAddress;
+
+    uint16_t                      id;
+    uint16_t                      nodesTotal;
     // updated on stable storage
-    uint64_t    term = 0;
-    uint8_t     votedFor = NULL;
-    bool        changing_state = false;
+    uint64_t                      currentTerm = 0;
+    uint16_t                      votedFor = 0; // no one should have ID = 0
+    std::vector<logEntry>         log;
 
     // not updated on stable storage
-    uint64_t    commitIndex = 0; // last committed
-    uint64_t    lastApplied = 0; // last applied to state machine
+    uint64_t                      commitIndex = 0; // last committed
+    uint64_t                      lastApplied = 0; // last applied to state machine
+    uint64_t                      lastCommitAckdToClient = 0;
 
-    // for candidates
-    uint8_t     votes = 0;
+    // for candidate
+    std::atomic_uint_fast8_t      votes;
 
-    // fot leaders
-    std::array<uint8_t>    nextIndex;  // next to send
-    std::array<uint8_t>    matchIndex; // known to be replicated
+    // for leaders
+    std::vector<uint64_t>         nextIndex;  // next to send
+    std::vector<uint64_t>         matchIndex; // known to be replicated
 
-    std::array<std::string> log;
+    // locks
+    std::shared_lock<std::shared_timed_mutex> stateSharedLock;
+    std::unique_lock<std::shared_timed_mutex> stateUniqueLock;
 
+    // xio stuff
+    struct xio_session_ops          xio_session_ops;
+    struct xio_session**            xio_sessions;
+    struct xio_server*              xio_server;
+    struct server_data*             xio_server_data;
+    struct session_data*            xio_session_data;
+    struct xio_session_params*      xio_params;
+    struct xio_connection_params*   xio_cparams;
+
+    // func
     static RaftLog& getInstance();
 
     // State-related methods
@@ -45,11 +78,34 @@ public:
     void becomeFollower();
     void becomeCandidate();
     void becomeLeader();
+    int  getState();
 
+    // StateMachine functions
+    void applyAllUncommitedLogs();
+    virtual void applyToStateMachine(uint64_t indexToApply); // override, if state machine needs action
+
+    // TODO:
+    // void saveLogToDrive();
+    void initXio();
+    void runXioLoop();
+
+    static std::map<int, string> parse_config_file(string filename);
 private:
     // deleting copy and assignment constructors
     RaftLog(RaftLog const&)        = delete;
     void operator=(RaftLog const&) = delete;
 };
+
+struct logEntry {
+    uint64_t term;
+    std::string entry;
+};
+
+struct session_data {
+    struct xio_context          *ctx;
+    struct xio_connection       *conn;
+};
+
+
 
 #endif //XIO_RAFT_RAFT_LOG_H
